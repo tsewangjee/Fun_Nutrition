3dca4abf5c2d8cac1e3e0d4f6808e839e7bb8731
Tsewang <tsewangjee@gmail.com>
2021-12-24 21:15:51 +0530

    initial commit

---
+++ Fun_Nutrition/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fun_Nutrition.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
