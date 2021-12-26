7e71845a2e1c08c638519c60d0240ee58263ed00
Tsewang <tsewangjee@gmail.com>
2021-12-26 18:41:16 +0530

    new menu tab added

--- Fun_Nutrition/Controllers/HomeController.cs
+++ Fun_Nutrition/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Fun_Nutrition.Controllers
             return View();
         }
 
+        public IActionResult Nutrition()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
