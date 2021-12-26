dd6220f9a611c426c1519a2f0e2aa3e4689105c1
Tsewang <tsewangjee@gmail.com>
2021-12-26 22:25:08 +0530

    passing dynamic value to the front end

--- Fun_Nutrition/Controllers/HomeController.cs
+++ Fun_Nutrition/Controllers/HomeController.cs
@@ -38,5 +38,10 @@ namespace Fun_Nutrition.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Display_Percentage()
+        {
+            ViewBag.Percentage = "0.1%";
+            return View();
+        }
     }
 }
