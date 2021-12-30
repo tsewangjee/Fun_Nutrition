b053ef91929ad43c31e49853b0b5e19c7315045a
Tsewang <tsewangjee@gmail.com>
2021-12-30 23:50:20 +0530

    add new menu to nav bar

--- Fun_Nutrition/Controllers/HomeController.cs
+++ Fun_Nutrition/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Fun_Nutrition.Controllers
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
