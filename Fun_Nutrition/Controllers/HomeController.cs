5be94ee5248aaa71a78ac1998f9754641df00a84
Tsewang <tsewangjee@gmail.com>
2021-12-31 20:12:40 +0530

    new page about us

--- Fun_Nutrition/Controllers/HomeController.cs
+++ Fun_Nutrition/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Fun_Nutrition.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
