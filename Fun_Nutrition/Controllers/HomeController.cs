0180461c89354222635f2b7a139cab4205c3532a
Tsewang <tsewangjee@gmail.com>
2022-01-09 18:55:28 +0530

    adding new page

--- Fun_Nutrition/Controllers/HomeController.cs
+++ Fun_Nutrition/Controllers/HomeController.cs
@@ -64,6 +64,11 @@ namespace Fun_Nutrition.Controllers
             return View();
         }
 
+        public IActionResult UsefulProducts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
