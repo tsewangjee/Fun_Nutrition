f48544af33d466e10e16826dcd0b386e68c6da7d
Tsewang <tsewangjee@gmail.com>
2022-01-10 21:02:31 +0530

    adding another controller for specific user login

---
+++ Fun_Nutrition/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fun_Nutrition.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
