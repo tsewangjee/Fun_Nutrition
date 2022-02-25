b3738164e1528d561e12a48c2e311e41f789a773
Tsewang <tsewangjee@gmail.com>
2022-02-25 22:22:19 +0530

    new commit

--- Fun_Nutrition/Controllers/UserController.cs
+++ Fun_Nutrition/Controllers/UserController.cs
@@ -8,5 +8,18 @@ namespace Fun_Nutrition.Controllers
         {
             return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Display_Percentage()
+        {
+            
+            double num = 0.02;
+            ViewBag.Percentage = num.ToString();
+            return View();
+        }
     }
 }
